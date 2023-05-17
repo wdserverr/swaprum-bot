5fdd17b9da638c680ff4b9dec1fd6edf96c1fc95
wdserverr <42107311+wdserverr@users.noreply.github.com>
2023-05-17 23:14:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import fetch from "node-fetch";
 import { address } from "./address.js";
-import cron from "node-cron";
 
 async function fetchData() {
     let headersList = {
         "Accept": "*/*",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36:"
     }
+
+    console.log(`\nMulai Mengeksekusi script..\n`)
 
     for (let i = 0; i < address.length; i++) {
         try {
@@ -20,14 +21,56 @@ async function fetchData() {
         } catch (error) {
             console.log(error.message);
         }
+        
     }
+    console.log(`\n####################################`)
+    console.log(`\nClaim Selesai\n`)
+    console.log(`####################################\n`)
+    Timer()
 }
 
-// Jadwalkan eksekusi fetchData setiap 1 jam
-cron.schedule("0 * * * *", async () => {
-    console.log("Fetching data...");
-    await fetchData();
-});
+async function Timer() {
+const currentTimestamp = Date.now()
+const calc = 60 * 60 * 1000 
+const targetTimestamp = currentTimestamp + calc 
+const oneSecond = 1000 
+let timeLeft = targetTimestamp - currentTimestamp
+let timer = timeLeft / 1000 / 60
 
-// Eksekusi fetchData sekali saat menjalankan script
-fetchData();
+const intervalId = setInterval(() => {
+    timeLeft -= oneSecond
+    console.log(
+        Math.floor(timeLeft / 1000 / 60 / 60),
+        'Jam',
+        Math.floor((timeLeft / 1000 / 60) % 60),
+        'Menit',
+        Math.floor((timeLeft / 1000) % 60),
+        'Detik remaining...'
+    )
+}, oneSecond)
+
+if (timeLeft >= 3600000) {
+    let hours = Math.floor(timeLeft / 1000 / 60 / 60)
+    console.log('Script akan dieksekusi lagi pada', hours, 'Jam')
+
+    setTimeout(() => {
+        clearInterval(intervalId)
+        main(`${hours} Jam`)
+    }, timeLeft)
+} else if (timeLeft >= 60000 && timeLeft < 3600000) {
+    console.log('Script akan dieksekusi lagi pada', timer, 'menit')
+    setTimeout(() => {
+        clearInterval(intervalId)
+        main(`${timer} menit`)
+    }, timeLeft)
+} else if (timeLeft < 60000) {
+    let second = timeLeft / 1000
+    console.log('Script akan dieksekusi lagi pada', second, 'detik')
+    setTimeout( async () => {
+        clearInterval(intervalId)
+        await fetchData();
+    }, timeLeft)
+}
+}
+
+fetchData()
